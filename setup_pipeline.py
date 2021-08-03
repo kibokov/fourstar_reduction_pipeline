0e1c1bcbb8632c59aabb41ef5394b6ac3e32a1c7
Viraj Manwadkar <44586619+kibokov@users.noreply.github.com>
2021-08-02 21:25:15 -0500

    changes to setup.py

---
+++ setup_pipeline.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+if __name__ == '__main__': 
+    
+    os.system('cd pipeline && git clone https://github.com/ysBach/imcombinepy.git')
+    os.system('python3 pipeline/imcombinepy/setup.py install')
+    os.system('mv -f pipeline/imcombinepy/imcombinepy/ pipeline/temppy')
+    os.system('rm -rf pipeline/imcombinepy/')
+    os.system('mv pipeline/temppy/ pipeline/imcombinepy')
+    
+
+
